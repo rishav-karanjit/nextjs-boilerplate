1dd42e5fc638bdd582d60d1967a85c44a58b2136
Rishav karanjit <karanjitrishav4@gmail.com>
2022-11-04 12:59:47 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> created by Clemson Username: rkaranj
+          Welcome to <a href="https://nextjs.org">Next.js!</a> 
         </h1>
       
+        <p className={styles.description}>
+          Created by Clemson Username: rkaranj
+        </p>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
